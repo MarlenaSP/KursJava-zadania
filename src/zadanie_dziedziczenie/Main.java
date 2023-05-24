7b9e79713ef28b0dd469804764f510e566f87f62
Marlena <sosnowska.marlena@wp.pl>
2023-05-24 20:34:03 +0200

    Klasy i dziedziczenie zadanie

---
+++ src/zadanie_dziedziczenie/Main.java
@@ -0,0 +1,8 @@
+package zadanie_dziedziczenie;
+
+public class Main {
+    public static void main(String[] args) {
+        Pracownik pracownik = new Pracownik("Jan", "Nowakowski", "magazynier", "XYZ");
+        pracownik.przedstawSie();
+    }
+}
